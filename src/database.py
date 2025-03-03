5ae0875f49a248937b632bab7f7cfc4c2b7f7729
Tiago <tiagoriely@gmail.com>
2025-03-03 16:20:58 +0000

    formatting database.py

--- src/database.py
+++ src/database.py
@@ -5,5 +5,5 @@ from src.config import Config
 
 
 def get_db_connection():
-    """Create and return a new database connection."""
+    """Create And return a new database connection."""
     return psycopg2.connect(Config.DATABASE_URL)
